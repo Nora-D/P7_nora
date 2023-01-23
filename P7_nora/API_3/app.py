b7a7b73d2d1e443676d5ae73d0644d1049684330
Nora-D <91324490+Nora-D@users.noreply.github.com>
2023-01-23 20:48:01 +0100

    Add files via upload

---
+++ P7_nora/API_3/app.py
@@ -0,0 +1,103 @@
+from flask import Flask
+import pandas as pd 
+import pickle as pk 
+import sklearn
+
+df = pd.read_csv("df_ok_flask.csv")
+
+model = pk.load(open('grid.pk', 'rb'))
+model2 = pk.load(open('X_G.pk', 'rb'))
+model3 = pk.load(open('XGB.pk', 'rb'))
+
+app=Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bonjour"
+    
+
+@app.route('/first')
+def first():
+    '''return first row
+    '''
+
+    ser = df.iloc[0]
+    print(ser)
+
+    dd = ser.to_dict()
+    print(dd)
+    
+    return str(dd)
+
+
+@app.route('/ids')
+def ids():
+    '''return a list containing all ids'''
+    list_ids = df.SK_ID_CURR.to_list()
+    
+
+
+
+    return str(list_ids[:10])
+
+
+
+
+
+
+
+
+
+@app.route('/row/<id>')
+def row(id):
+    '''get specific row
+    '''
+    try :
+        id = int(id)
+    except :
+        return "Il faut un nombre!"
+
+    ser = df.loc[df["SK_ID_CURR"] == id]
+    print(len(ser))
+    if len(ser) == 0:
+        return "ça marche po!"
+    ser = ser.iloc[0]
+    print(ser)
+
+
+
+    dd = ser.to_dict()
+    print(dd)
+    
+    return str(dd)
+
+
+@app.route('/predict/<id>')
+def predict(id):
+    '''return a prediction linked to a given id '''
+    try :
+        id = int(id)
+    except :
+        return "Il faut un nombre!"
+
+    ser = df.loc[df["SK_ID_CURR"] == id]
+    print(len(ser))
+    if len(ser) == 0:
+        return "ça marche po!"
+    #ser = ser.iloc[0]
+    print(ser)
+    ser.drop(columns = ["SK_ID_CURR"], inplace = True)
+    #ser = ser.values.reshape(-1, 1)
+    #print(ser)
+    prediction = model3.predict(ser)
+    
+
+    if prediction == 1:
+        return "Le crédit est malheureusement refusé"
+    else:
+        return "Le crédit est accépté !"
+
+
+
+if __name__=="__main__":
+    app.run(debug = True, port = 5000, host = "0.0.0.0")
---
+++ P7_nora/API_3/front.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import requests
+import pandas as pd
+from PIL import Image
+
+
+base = "http://172.104.245.43"
+
+
+st.write('''# Bienvenue,
+cette application prédit l'obtention d'un prêt bancaire
+''')
+
+def user_input():
+    '''Récupérer l'ensemble des ids de la BBD'''
+
+    r = requests.get(base + '/ids')
+    assert int(r.status_code) == 200
+
+    ids = eval(r.text)
+    return ids
+
+def user_input():
+    '''Récupérer l'ensemble des ids de la BBD'''
+
+    r = requests.get(base + '/ids')
+    assert int(r.status_code) == 200
+
+    ids = eval(r.text)
+    return ids
+
+def get_informations(ids):
+    """get informations of a client based on his id"""
+
+    r = requests.get(base + "/row/" + str(ids) + "")
+    assert int(r.status_code) == 200
+
+    info = eval(r.text)
+    info = pd.DataFrame.from_dict(info, orient='index')
+    infos = st.table(info)
+
+def predict(ids):
+    """predict and id"""
+
+    r = requests.get(base + "/predict/" + str(ids) + "")
+    assert int(r.status_code) == 200
+
+    #problème d'encodage?
+    ans = r.text
+
+    assert ans in ["Le crédit est malheureusement refusé", "Le crédit est accépté !"]
+    return ans
+
+
+
+# selection
+list_ids = user_input()
+option2 = st.selectbox("Qui est le client?", list_ids)
+st.write("Vous avez sélectionné:", option2)
+
+print(option2)
+
+st.write("Les informations du client sont :")
+# Print information of a client
+info_client = get_informations(option2)
+#st.write("Vos informations sont:", infos)
+
+# pred
+#if isinstance(option2, int):
+    #ans = predict(option2)
+    #ans = "Le crédit est accépté !" if ans else "Le crédit est malheureusement refusé"
+
+ans = predict(option2)
+st.write(f"La réponse est : {ans}.")
+st.write("Les explications de cette décision sont données par les deux graphiques ci dessous.")
+
+
+
+# Plot!
+#st.plotly_chart(fig, use_container_width=True)
+
+#opening the image
+
+image = Image.open('shap.png')
+
+def shap_glob:
+    response = requests.get(base + '/shap')
+
+    file = open("sample_image.png", "wb")
+    file.write(response.content)
+    file.close()
+
+
+#displaying the image on streamlit app
+st.write("Ce premier graphique montre l'importance des informations (par ordre d'importance) dans l'établissement du modèle d'optention du crédit")
+st.image(image)
